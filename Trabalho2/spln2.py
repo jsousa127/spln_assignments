7fc4cd428db32d4d1b75ae6f8d139d9e2bd11e0e
filipelpaquito30 <filipe.v.f@hotmail.com>
2018-11-30 02:10:56 +0000

    live

--- Trabalho2/spln2.py
+++ Trabalho2/spln2.py
@@ -6,29 +6,29 @@ from sqlalchemy import create_engine, Column, Table, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
-# def lookup(word,pal):
-#     global array
-#     if not word:
-#         return 0
+def lookup(word,pal):
+    global array
+    if not word:
+        return 0
         
-#     for e in session.query(Elemento):
-#         el = e.elem.lower()
-#         if re.match(el, word) :
-#             if (len(word) == len(el)) | (lookup(word[len(el):],pal) == 1):
-#                 pal.elementos.append(e)
-#                 e.palavras.append(pal)
-#                 return 1
+    for e in session.query(Elemento):
+        el = e.elem.lower()
+        if re.match(el, word) :
+            if (len(word) == len(el)) | (lookup(word[len(el):],pal) == 1):
+                pal.elementos.append(e)
+                e.palavras.append(pal)
+                return 1
 
-#     return 0
+    return 0
 
-# def strip_accents(word):
-#    return ''.join(c for c in unicodedata.normalize('NFD', word)
-#                   if unicodedata.category(c) != 'Mn')
+def strip_accents(word):
+   return ''.join(c for c in unicodedata.normalize('NFD', word)
+                  if unicodedata.category(c) != 'Mn')
 
-# def populate_elements():
-#     elements = open("elements.txt","r").read().split()
-#     for e in elements:
-#         session.add(Elemento(elem=e))
+def populate_elements():
+    elements = open("elements.txt","r").read().split()
+    for e in elements:
+        session.add(Elemento(elem=e))
 
 
 Base= declarative_base()
@@ -55,22 +55,22 @@ engine= create_engine('sqlite:///spln.db', echo=True)
 Base.metadata.create_all(bind=engine)
 Session = sessionmaker(bind=engine)
 session = Session()
-# populate_elements()
-# f = open(argv[1],"r")
-# try:
-#     words = f.read().split()
-# except:
-#     try:
-#         import codecs
-#         words = codecs.open(argv[1],"r","iso-8859-1").read().split()
-#     except:
-#         print("Não foi possivel abrir o ficheiro")
+populate_elements()
+f = open(argv[1],"r")
+try:
+    words = f.read().split()
+except:
+    try:
+        import codecs
+        words = codecs.open(argv[1],"r","iso-8859-1").read().split()
+    except:
+        print("Não foi possivel abrir o ficheiro")
 
-# for w in words:
-#     word = w.lower()
-#     pal = Palavras(pal=word)
-#     if lookup(word,pal):
-#         session.add(pal)
+for w in words:
+    word = w.lower()
+    pal = Palavras(pal=word)
+    if lookup(word,pal):
+        session.add(pal)
 
 
 query1 = session.query(Elemento).filter(Elemento.elem == 'Ba').first().palavras
@@ -81,5 +81,5 @@ for word in query1 :
 
 
 
-# session.commit()
-# session.close()
+session.commit()
+session.close()
