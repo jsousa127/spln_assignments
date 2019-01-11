1b585c60c077eded197d257edd724dd047a7a2c2
José Sousa <josesousa@Joses-MBP-2.lan>
2019-01-11 02:04:34 +0000

    pretty code and comments

--- 3thAssignment/main.py
+++ 3thAssignment/main.py
@@ -1,14 +1,17 @@
 from math import log
-import re
+from re import sub
+
 # Build a cost dictionary, assuming Zipf's law and cost = -math.log(probability).
 words = open("dictionary.txt").read().split()
 wordcost = dict((k, log((i+1)*log(len(words)))) for i,k in enumerate(words))
 maxword = max(len(x) for x in words)
 
+# Open input file and remove any mistaken spaces
+input = open("file.txt","r").read().replace(" ","")
+
 def add_spaces(s):
-    # Find the best match for the i first characters, assuming cost has
+    # Find the best pair (match_cost, match_length) for the i first characters, assuming cost has
     # been built for the i-1 first characters.
-    # Returns a pair (match_cost, match_length).
     def best_match(i):
         candidates = enumerate(reversed(cost[max(0, i-maxword):i]))
         ls = s.lower()
@@ -31,7 +34,11 @@ def add_spaces(s):
 
     return " ".join(reversed(out))
 
-input = open("file.txt","r").read().replace(" ","")
-output = re.sub(r'\s([?.!,:;](?:\s|$))', r'\1', add_spaces(input))
-output = re.sub(r'(["(])\s(.*)\s([")])', r'\1\2\3', output)
+# Add spaces between words
+aux = add_spaces(input)
+# Remove spaces before any of this -> . ? ! , : ;
+aux2 = sub(r'\s([?.!,:;](?:\s|$))', r'\1', aux)
+# Remove spaces after this " or ( and before " or )
+output = sub(r'(["(])\s(.*)\s([")])', r'\1\2\3', aux2)
+
 print(output)    
